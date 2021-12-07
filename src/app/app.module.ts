69422715c46cd05a8e0969114ecb283143d6ac5d
Mouheb <mouhebmh@gmail.com>
2021-12-07 18:23:25 +0100

    Add components and work in User component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,48 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { UserComponent } from './modules/User/user/user.component';
+import { ProduitComponent } from './modules/Produit/produit/produit.component';
+import { StockComponent } from './modules/Stock/stock/stock.component';
+import { FactureComponent } from './modules/Facture/facture/facture.component';
+import { FournisseurComponent } from './modules/Fournisseur/fournisseur/fournisseur.component';
+import { UserAuthComponent } from './modules/Auth/user-auth/user-auth.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-    
+    UserComponent,
+    ProduitComponent,
+    StockComponent,
+    FactureComponent,
+    FournisseurComponent,
+    UserAuthComponent,
+ 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DefaultModule
+    DefaultModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgbModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/Services/user-service.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Users } from '../Model/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+  url=environment.hostUrl;
+  
+  constructor(private http:HttpClient) { }
+  
+
+  getAllUsers(){
+    return this.http.get(this.url+"/user/getAllUsers",{observe:"response"});
+  }
+}
